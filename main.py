228ecbb061acaf1f2f8a71f27757b6f44a21d451
MidKnightFox <mail@davidvasica.net>
2025-03-24 01:01:39 +1100

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+def main():
+    # Prints the contents of the book to console
+    print(get_book_text("books/frankenstein.txt"))
+
 # Gets the file path as input and returns contents of a file as a string
 def get_book_text(b):
     with open(b) as book:
         return book.read()
 
-def main():
-    print(get_book_text("books/frankenstein.txt"))
+
 
 main()
