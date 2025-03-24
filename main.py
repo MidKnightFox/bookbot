f2db46ff9e49c5679a28e12a5b506ec8eebdaeb2
MidKnightFox <mail@davidvasica.net>
2025-03-24 17:09:23 +1100

    Updated main.py for bookbot CH2L4

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 def main():
-    # Prints the contents of the book to console
-    print(get_book_text("books/frankenstein.txt"))
+    book_path = "books/frankenstein.txt"
+    print_text = get_book_text(book_path)
+    count_words = count_book_text(book_path)
+
+    print(f"{count_words} words found in the document")
 
 # Gets the file path as input and returns contents of a file as a string
 def get_book_text(b):
     with open(b) as book:
         return book.read()
 
+#Counts number of words in the input string
+def count_book_text(text):
+    word_count = 0
+    with open(text) as words:
+        string = words.read()
+        word_list  = string.split()
 
+        for w in word_list:
+            word_count += 1
+    return word_count
 
 main()
