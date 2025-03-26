b7314dba294bd8ff38390dda065b8ca6580b16ee
MidKnightFox <mail@davidvasica.net>
2025-03-27 00:16:34 +1100

    fixed char count func in stats.py

--- stats.py
+++ stats.py
@@ -14,9 +14,12 @@ def count_book_char(text):
     char_count = 0
     char_dict = {}
     with open(text) as words:
-        lower_char = words.lower()
-        chars_list = list(lower_char)
+        string = words.read()
+        chars_list = list(string.lower())
         for char in chars_list:
-            char_dict[char] += 1
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                 char_dict[char] = 1
 
     return char_dict
