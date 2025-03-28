67407e596f19bcc12736f8a36e27c8b7cd49fc27
MidKnightFox <mail@davidvasica.net>
2025-03-28 21:35:14 +1100

    Updated main.py to accept args

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from stats import count_book_text
-from stats import count_book_char
-from stats import sort_book_char
+from stats import count_book_text, count_book_char, sort_book_char
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
-    #print_text = get_book_text(book_path)
-    count_words = count_book_text(book_path) # count number of words in the input text
-    count_char = count_book_char(book_path) # count number of characters in the input text
-    sorted_char_count = sort_book_char(count_char) # Generate report
-    
-    #Print results/report
-    print(f'''============ BOOKBOT ============
+    if len(sys.argv) > 1:    
+        book_path = sys.argv[1]
+        count_words = count_book_text(book_path) # count number of words in the input text
+        count_char = count_book_char(book_path) # count number of characters in the input text
+        sorted_char_count = sort_book_char(count_char) # Generate report
+        
+        #Print results/report
+        print(f'''============ BOOKBOT ============
 Analyzing book found at {book_path}...
 ----------- Word Count ----------
 Found {count_words} total words
 --------- Character Count -------''')
-    for char_data in sorted_char_count:
-        if char_data["char"].isalpha():
-            print(f"{char_data['char']}: {char_data['count']} ")
-    print("============= END ===============")
-    
+        for char_data in sorted_char_count:
+            if char_data["char"].isalpha():
+                print(f"{char_data['char']}: {char_data['count']} ")
+        print("============= END ===============")
 
-
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
 # Gets the file path as input and returns contents of a file as a string
 #def get_book_text(b):
 #    with open(b) as book:
